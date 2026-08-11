ae14ccb0ac6f92363c4c58434fa012df72860531
leejwnspect-star <lee.j.wnspect@gmail.com>
2026-08-11 22:51:53 +0900

    Update SuggestionBoxApplication.java

--- src/main/java/com/example/suggestionbox/SuggestionBoxApplication.java
+++ src/main/java/com/example/suggestionbox/SuggestionBoxApplication.java
@@ -3,13 +3,17 @@ package com.example.suggestionbox;
 import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.suggestionbox")
+@EntityScan(basePackages = "com.example.suggestionbox")
 @RestController
 public class SuggestionBoxApplication {
 
